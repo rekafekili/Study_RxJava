fe5e64eab1207aecd615370de564778cdb999e06
Rekafekili <rekafekili@gmail.com>
2020-07-22 20:32:29 +0900

    HelloWorldSample.java

--- src/main/java/HelloWorldSample.java
+++ src/main/java/HelloWorldSample.java
@@ -2,6 +2,10 @@ import io.reactivex.rxjava3.core.Flowable;
 
 public class HelloWorldSample {
     public static void main(String[] args) {
-        Flowable.just("Hello World").subscribe(System.out::println);
+        // 데이터를 통지하는 생산자를 생성한다.
+        Flowable<String> flowable = Flowable.just("Hello", "World");
+
+        // 통지 받은 데이터를 출력한다.
+        flowable.subscribe(data -> System.out.println(data));
     }
 }
